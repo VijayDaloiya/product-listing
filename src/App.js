9911dc343f733c30eee5aa1390cd52a1a715eae0
VijayDaloiya <63780543+VijayDaloiya@users.noreply.github.com>
2021-08-29 12:28:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+import { PostList } from './productlist.js';
+
+const App = () => (
+    <Admin dataProvider={jsonServerProvider('https://jsonplaceholder.typicode.com')}>
+        <Resource name="posts" list={PostList} />
+    </Admin>
+);
+
+export default App;
